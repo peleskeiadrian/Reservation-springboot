9a62488544fc3270b2e96a3851ce2af28f1de5e6
Peleskei Janos Adrian <peleskeiadrian@gmail.com>
2024-10-16 10:02:46 +0200

    After framework installation

---
+++ Reservation-Springboot/src/main/java/com/example/reservationspringboot/ReservationSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.reservationspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationSpringbootApplication.class, args);
+    }
+
+}
