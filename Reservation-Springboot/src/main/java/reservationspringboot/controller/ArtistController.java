2f1ccf3750016086ed99c03ddee4043c8e037cf1
Peleskei Janos Adrian <peleskeiadrian@gmail.com>
2024-11-08 17:32:28 +0100

    Form update Artist

--- Reservation-Springboot/src/main/java/reservationspringboot/controller/ArtistController.java
+++ Reservation-Springboot/src/main/java/reservationspringboot/controller/ArtistController.java
@@ -5,11 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import reservationspringboot.model.Artist;
 import reservationspringboot.service.ArtistService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+
 
 @Controller
 public class ArtistController {
@@ -35,6 +41,44 @@ public class ArtistController {
 
         return "artists/show";
     }
+
+    @GetMapping("/artists/{id}/edit")
+    public String edit(Model model, @PathVariable("id") long id, HttpServletRequest request) {
+        Artist artist = service.getArtist(id);
+
+        model.addAttribute("artist", artist);
+
+
+        //Générer le lien retour pour l'annulation
+        String referrer = request.getHeader("Referer");
+
+        if(referrer!=null && !referrer.equals("")) {
+            model.addAttribute("back", referrer);
+        } else {
+            model.addAttribute("back", "/artists/"+artist.getId());
+        }
+
+        return "artists/edit";
+    }
+
+    @PutMapping("/artists/{id}/edit")
+    public String update(@Valid @ModelAttribute("artist") Artist artist, BindingResult bindingResult, @PathVariable("id") long id, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "artists/edit";
+        }
+
+        Artist existing = service.getArtist(id);
+
+        if(existing==null) {
+            return "artists/index";
+        }
+
+        service.updateArtist(id, artist);
+
+        return "redirect:/artists/"+artist.getId();
+    }
+
 
 }
 
--- Reservation-Springboot/src/main/java/reservationspringboot/model/Artist.java
+++ Reservation-Springboot/src/main/java/reservationspringboot/model/Artist.java
@@ -5,6 +5,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 
 @Entity
 @Table(name="artists")
@@ -12,10 +15,17 @@ public class Artist {
   @Id
   @GeneratedValue(strategy=GenerationType.IDENTITY)
   private Long id;
+
+  @NotBlank(message = "The firstname must not be empty.")
+  @Size(min=2, max=60, message = "The firstname must be between 2 and 60 characters long.")
   private String firstname;
+
+  @NotBlank(message = "The lastname must not be empty.")
+  @Size(min=2, max=60, message = "The firstname must be between 2 and 60 characters long.")
   private String lastname;
 
   protected Artist() {}
+
 
   public Artist(String firstname, String lastname) {
     this.firstname = firstname;
