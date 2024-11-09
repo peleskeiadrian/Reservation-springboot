d69eb1e2845b500eda805f6fc0ccc25c511b1e96
Peleskei Janos Adrian <peleskeiadrian@gmail.com>
2024-11-09 14:01:32 +0100

    Form Create &  Delete Artist

--- Reservation-Springboot/src/main/java/reservationspringboot/controller/ArtistController.java
+++ Reservation-Springboot/src/main/java/reservationspringboot/controller/ArtistController.java
@@ -2,19 +2,22 @@ package reservationspringboot.controller;
 
 import java.util.List;
 
+import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
 import reservationspringboot.model.Artist;
 import reservationspringboot.service.ArtistService;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
 
 
 @Controller
@@ -78,6 +81,40 @@ public class ArtistController {
 
         return "redirect:/artists/"+artist.getId();
     }
+
+    @GetMapping("/artists/create")
+    public String create(Model model) {
+        Artist artist = new Artist(null,null);
+
+        model.addAttribute("artist", artist);
+
+        return "artists/create";
+    }
+
+    @PostMapping("/artists/create")
+    public String store(@Valid @ModelAttribute("artist") Artist artist, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "artists/create";
+        }
+
+        service.addArtist(artist);
+
+        return "redirect:/artists/"+artist.getId();
+    }
+
+
+    @DeleteMapping("/artists/{id}")
+    public String delete(@PathVariable("id") long id, Model model) {
+        Artist existing = service.getArtist(id);
+
+        if(existing!=null) {
+            service.deleteArtist(id);
+        }
+
+        return "redirect:/artists";
+    }
+
 
 
 }
