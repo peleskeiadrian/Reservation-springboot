372a14dbbcbd832fba987b788acfac761cf23b00
Peleskei Janos Adrian <peleskeiadrian@gmail.com>
2024-11-11 12:11:54 +0100

    Spring Security configuration

---
+++ Reservation-Springboot/src/main/java/reservationspringboot/config/CustomUserDetailsService.java
@@ -0,0 +1,38 @@
+package reservationspringboot.config;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import reservationspringboot.model.User;
+import reservationspringboot.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByLogin(username);
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getLogin(), user.getPassword(),
+                getGrantedAuthorities(user.getRole()));
+    }
+
+    @NotNull
+    private List<GrantedAuthority> getGrantedAuthorities(String role) {
+        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+        return authorities;
+    }
+}
+
