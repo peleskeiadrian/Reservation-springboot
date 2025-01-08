d79263e2d438e024b6ca5096157f82813d873e78
Peleskei Janos Adrian <peleskeiadrian@gmail.com>
2025-01-08 10:32:50 +0100

    Update templates Show (added artists’ list)

--- Reservation-Springboot/src/main/java/reservationspringboot/controller/ShowController.java
+++ Reservation-Springboot/src/main/java/reservationspringboot/controller/ShowController.java
@@ -1,11 +1,16 @@
 package reservationspringboot.controller;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import reservationspringboot.model.Artist;
+import reservationspringboot.model.ArtistType;
 import reservationspringboot.model.Show;
 import reservationspringboot.service.ShowService;
 
@@ -29,11 +34,26 @@ public class ShowController {
     public String show(Model model, @PathVariable("id") String id) {
         Show show = service.get(id);
 
+        //Récupérer les artistes du spectacle et les grouper par type
+        Map<String, ArrayList<Artist>> collaborateurs = new TreeMap<>();
+
+        for(ArtistType at : show.getArtistTypes()) {
+            String type = at.getType().getType();
+
+            if(collaborateurs.get(type) == null) {
+                collaborateurs.put(type, new ArrayList<>());
+            }
+
+            collaborateurs.get(type).add(at.getArtist());
+        }
+
+        model.addAttribute("collaborateurs", collaborateurs);
         model.addAttribute("show", show);
         model.addAttribute("title", "Fiche d'un spectacle");
 
         return "show/show";
     }
+
 
 }
 
